786b70838c9713c6ac86d246a72d75781ea50b54
Bruno-Özen <112650688+Bruno-Ozen@users.noreply.github.com>
2022-09-10 09:53:13 -0300

    Add files via upload

---
+++ Algoritmos em C#/Reversor_de_palavras/Reversor_de_palavras/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Reversor_de_palavras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //REVERSOR DE PALAVRAS
+            string palavra = "";
+            char[] nome;
+            char aux;
+            int indice = 1;
+            Console.WriteLine("Digite uma palavra: ");
+            palavra = Console.ReadLine();
+            nome = palavra.ToCharArray();
+            for (int cont1 = 0; cont1 < palavra.Length; cont1++)
+            {
+                for (int cont2 = 0; cont2 < palavra.Length - indice; cont2++)
+                {
+                    aux = nome[cont2];
+                    nome[cont2] = nome[cont2 + 1];
+                    nome[cont2 + 1] = aux;
+                }
+                indice++;
+            }
+            palavra = new string(nome);
+            Console.WriteLine(palavra);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algoritmos em C/Bubblesort/Bubblesort.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <locale.h>
+#include <iostream>
+
+//ORGANIZADOR DE VETORES (Ordem crescente)
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+    int vetor[5];
+    int aux;
+    //Leitura dos valores
+    for(int cont = 0; cont<5; cont++){
+        printf("Digite um valor: ");
+        scanf("%d", &vetor[cont]);
+    }
+    //Bubble sort
+    for(int n = 0; n<5; n++){
+        for(int cont = 0; cont<4; cont++){
+            if(vetor[cont] > vetor[cont+1]){
+                aux = vetor[cont];
+                vetor[cont] = vetor[cont+1];
+                vetor[cont+1] = aux;
+            }
+        }
+    }
+    //Saída dos dados
+    for(int cont = 0; cont < 5; cont++){
+        printf("Posição %d= %d\n", cont, vetor[cont]);
+    }
+    system("pause");
+    return 0;
+}
