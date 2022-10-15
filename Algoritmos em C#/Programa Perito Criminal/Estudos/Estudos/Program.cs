417ec41afd249e4ef250eb36f47f19229b592523
Bruno-Özen <112650688+Bruno-Ozen@users.noreply.github.com>
2022-10-15 13:20:09 -0300

    Add files via upload

---
+++ Algoritmos em C#/Programa Perito Criminal/Estudos/Estudos/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+public class Estudos
+{
+    public static void Main(string[] args)
+    {
+        //PROGRAMA PERITO CRIMINAL
+        int contRespostas = 0;
+        ConsoleKeyInfo key;
+        WriteLine("Telefonou para a vítima? (s/n)");
+        key = ReadKey();
+        if (key.Key == ConsoleKey.S)
+            contRespostas++;
+        WriteLine("Esteve no local do crime? (s/n)");
+        key = ReadKey();
+        if (key.Key == ConsoleKey.S)
+            contRespostas++;
+        WriteLine("Mora perto da vítima? (s/n)");
+        key = ReadKey();
+        if (key.Key == ConsoleKey.S)
+            contRespostas++;
+        WriteLine("Devia para a vítima? (s/n)");
+        key = ReadKey();
+        if (key.Key == ConsoleKey.S)
+            contRespostas++;
+        WriteLine("Já trabalhou com a vítima? (s/n)");
+        key = ReadKey();
+        if (key.Key == ConsoleKey.S)
+            contRespostas++;
+        if (contRespostas == 2)
+        {
+            WriteLine("Suspeito.");
+        }
+        else if (contRespostas >= 3 && contRespostas <= 4)
+        {
+            WriteLine("Cúmplice.");
+        }
+        else if (contRespostas == 5)
+        {
+            WriteLine("Assassino.");
+        }
+        else
+        {
+            WriteLine("Inocente.");
+        }
+        ReadLine();
+    }
+}
